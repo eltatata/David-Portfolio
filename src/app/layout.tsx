f632ef4796445a9ff9fdd58227d51f18ecc2c509
eltatataa <davidtowo75@gmail.com>
2025-08-19 10:47:55 -0500

    feat: dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Particles from '@/components/particles';
+import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
 
 const geistSans = Geist({
@@ -25,11 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="relative z-10 w-full">{children}</div>
+        <div className="relative z-10 w-full">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </div>
         <div className="fixed inset-0 w-full h-full -z-10">
           <Particles
             particleColors={['#00FF00']}
