ce6f8afcd4a6e504fc1907532c1e9bc985371920
eltatataa <davidtowo75@gmail.com>
2025-08-20 00:50:07 -0500

    feat: logo info

---
+++ src/lib/logos-info.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  SiGit,
+  SiJest,
+  SiReact,
+  SiDocker,
+  SiPrisma,
+  SiVercel,
+  SiMongodb,
+  SiLangchain,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiTypescript,
+  SiPostgresql,
+  SiTailwindcss,
+} from 'react-icons/si';
+import { FaAws } from 'react-icons/fa';
+
+export const logosInfo = [
+  {
+    node: <SiNodedotjs />,
+    title: 'Node.js',
+    href: 'https://nodejs.org',
+  },
+  {
+    node: <SiTypescript />,
+    title: 'TypeScript',
+    href: 'https://www.typescriptlang.org',
+  },
+  {
+    node: <SiNextdotjs />,
+    title: 'Next.js',
+    href: 'https://nextjs.org',
+  },
+  {
+    node: <SiReact />,
+    title: 'React',
+    href: 'https://react.dev',
+  },
+  {
+    node: <SiTailwindcss />,
+    title: 'Tailwind CSS',
+    href: 'https://tailwindcss.com',
+  },
+  {
+    node: <SiGit />,
+    title: 'Git',
+    href: 'https://git-scm.com',
+  },
+  {
+    node: <SiMongodb />,
+    title: 'MongoDB',
+    href: 'https://www.mongodb.com',
+  },
+  {
+    node: <SiPostgresql />,
+    title: 'PostgreSQL',
+    href: 'https://www.postgresql.org',
+  },
+  {
+    node: <SiPrisma />,
+    title: 'Prisma',
+    href: 'https://www.prisma.io',
+  },
+  {
+    node: <SiDocker />,
+    title: 'Docker',
+    href: 'https://www.docker.com',
+  },
+  {
+    node: <FaAws />,
+    title: 'AWS',
+    href: 'https://aws.amazon.com',
+  },
+  {
+    node: <SiVercel />,
+    title: 'Vercel',
+    href: 'https://vercel.com',
+  },
+  {
+    node: <SiLangchain />,
+    title: 'LangChain',
+    href: 'https://langchain.com',
+  },
+  {
+    node: <SiJest />,
+    title: 'Jest',
+    href: 'https://jestjs.io',
+  },
+];
