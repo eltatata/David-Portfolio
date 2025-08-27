c3928ec9962b1541602438cb7908d9e8f9519e3a
eltatata <davidtowo75@gmail.com>
2025-08-27 16:02:19 -0500

    feat: connection settings and data ingestion to the vector store

---
+++ src/database/db-connection.ts
@@ -0,0 +1,28 @@
+import { MongoDBAtlasVectorSearch } from '@langchain/mongodb';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { MongoClient } from 'mongodb';
+
+if (
+  !process.env.MONGODB_ATLAS_URI ||
+  !process.env.DATABASE_NAME ||
+  !process.env.COLLECTION_NAME
+) {
+  throw new Error(
+    'Missing MONGODB_ATLAS_URI, DATABASE_NAME or COLLECTION_NAME environment variable',
+  );
+}
+
+const client = new MongoClient(process.env.MONGODB_ATLAS_URI);
+const collection = client
+  .db(process.env.DATABASE_NAME)
+  .collection(process.env.COLLECTION_NAME || 'data');
+
+export const vectorStore = new MongoDBAtlasVectorSearch(
+  new OpenAIEmbeddings(),
+  {
+    collection,
+    indexName: 'vector_index',
+    textKey: 'text',
+    embeddingKey: 'embedding',
+  },
+);
